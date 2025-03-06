83c3d5982cb01886c5b058bb434af846c44c805c
Zulfizar Arziqulovna <arziqulovnaz@gmail.com>
2025-03-06 15:49:03 +1000

    dev: changed code to make more efficient readdabilty

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ online_reatil_data = r'C:/Users/user/Desktop/Customer-Lifetime-Value-Prediction/
 df = pd.read_excel(online_reatil_data)
 df.dropna(inplace=True)
 df.drop_duplicates(inplace=True)
-df['Quantity'] = df['Quantity'].astype(float)
-
+df["TotalAmount"] = df["Quantity"] * df["UnitPrice"]
 # Visualize TotalAmount distribution
-sns.histplot(df["UnitPrice"], bins=30, kde=True)
+sns.histplot(df["TotalAmount"], bins=30, kde=True)
 plt.title("Distribution of Total Spend per Customer")
-# plt.show()
+plt.show()
 
 # the most money spent curstomers' top 10 list
-customer_spending = df.groupby("CustomerID")["UnitPrice"].sum().sort_values(ascending=False)
+customer_spending = df.groupby("CustomerID")["TotalAmount"].sum().sort_values(ascending=False)
 
 
 # compute rfm(recency, frequency, monetay) features
@@ -26,23 +25,13 @@ reference_date = df["InvoiceDate"].max()
 rfm = df.groupby("CustomerID").agg({
     "InvoiceDate": lambda x: (reference_date - x.max()).days,  # Recency (days since last purchase)
     "InvoiceNo": "count",  # Frequency (total purchases)
-    "UnitPrice": "sum"  # Monetary Value (total spending)
+    "TotalAmount": "sum"  # Monetary Value (total spending)
 })
 
 rfm.columns = ["Recency", "Frequency", "Monetary"]
 rfm.head()
 
-# Recency Distribution
-sns.histplot(rfm["Recency"], bins=30, kde=True)
-plt.title("Recency Distribution")
-plt.show()
-
-# Frequency Distribution
-sns.histplot(rfm["Frequency"], bins=30, kde=True)
-plt.title("Frequency Distribution")
-plt.show()
-
-# Monetary Value Distribution
-sns.histplot(rfm["Monetary"], bins=30, kde=True)
-plt.title("Monetary Value Distribution")
-plt.show()
+for col in rfm.columns:
+    sns.histplot(rfm[col], bins=30, kde=True)
+    plt.title(f"{col} Distribution")
+    plt.show()
